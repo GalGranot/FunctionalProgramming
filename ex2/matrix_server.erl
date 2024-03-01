8e5c611e26d6a6f1aee535b6874f12ebd1099491
GalGranot <gal.granot95@gmail.com>
2024-03-01 19:57:59 +0200

    undefined funtions for some reason

--- ex2/matrix_server.erl
+++ ex2/matrix_server.erl
@@ -1,6 +1,6 @@
 -module(matrix_server).
 -import(matrix, [getZeroMat/2, getRow/2, getCol/2, setElementMat/4, getSizes/2]).
--export([scalarProduct/2, mult/4, start_server/0, serverLoop/1]).
+-export([mult/2, start_server/0, serverLoop/1, shutdown/0, explanation/0]).
 
 scalarProduct(V1, V2) ->
     MultiplePairs = lists:zip(V1, V2),
@@ -12,12 +12,17 @@ vectorMultiply(V1, V2, RowNum, ColNum, ServerPid) ->
     ServerPid ! {Result, RowNum, ColNum}.
 
 mult(Mat1, Mat2, MsgRef, Pid) ->
+    io:format("Here1~n"),
     {RowsNum, _, ColsNum} = getSizes(Mat1, Mat2),
+    io:format("Here2~n"),
     MatResult = matrix:getZeroMat(RowsNum, ColsNum),
+    io:format("Here3~n"),
     [spawn(?MODULE, vectorMultiply, % args for multiplication
     [getRow(Mat1, RowIndex), getCol(Mat2, ColIndex), RowIndex, ColIndex, self()])
     || RowIndex <- lists:seq(1, RowsNum), ColIndex <- lists:seq(1, ColsNum)],
+    io:format("Here4~n"),
     JobsToDo = RowsNum * ColsNum,
+    io:format("Here5~n"),
     gatherElements(MatResult, 0, JobsToDo, Pid, MsgRef).
 
 gatherElements(MatResult, JobsDone, JobsMax, Pid, MsgRef) ->
@@ -30,18 +35,16 @@ gatherElements(MatResult, JobsDone, JobsMax, Pid, MsgRef) ->
             Pid ! {result, MsgRef, MatResult}
     end.
 
-% mult(Mat1, Mat2) ->
-%     MsgRef = make_ref(),
-%     matrix_server ! {self(), MsgRef, {multiple, Mat1, Mat2}},
-%     receive
-%         {result, MsgRef, Mat} -> Mat;
-%         _ -> io:format("Unknown result format, aborting~n")
-%     end.
+mult(Mat1, Mat2) ->
+    MsgRef = make_ref(),
+    matrix_server ! {self(), MsgRef, {multiple, Mat1, Mat2}},
+    receive
+        {result, MsgRef, Mat} -> Mat
+    end.
 
 start_server() ->
     spawn(matrix_supervisor, spawnServer, []),
-    io:format("Starting server~n"),
-    ok.
+    io:format("Hello from start_server~n").
 
 shutdown() ->
     matrix_server ! shutdown.
@@ -55,21 +58,27 @@ get_version() ->
 
 serverLoop(VersionNum) ->
     Version = "version_" ++ VersionNum,
-    io:format("Hello from serverLoop~n"),
+    % io:format(Version),
+    % io:format("Hello from serverLoop, version ~s~n", [integer_to_list(VersionNum)]),
     receive
         shutdown ->
             unregister(matrix_server),
             ok;
         sw_upgrade -> 
             ?MODULE:serverLoop(VersionNum + 1);
-        {Pid, MsgRef, {mutliple, Mat1, Mat2}} ->
-            spawn(?MODULE, mult, [Mat1, Mat2, MsgRef, Pid]),
+        {Pid, MsgRef, {multiple, Mat1, Mat2}} ->
+            io:format("Received multiplying command~n"),
+            spawn(?MODULE, mult, [Mat1, Mat2, MsgRef, Pid]), %%problematic line
+            io:format("Created proccess for mult"),
             serverLoop(Version);
         {Pid, MsgRef, get_version} ->
             Pid ! {MsgRef, Version},
             serverLoop(Version);
         _ ->
-            io:format("Unknown message format, ignoring"),
+            io:format("Unknown message format, ignoring~n"),
             serverLoop(Version)
     end.
 
+explanation() ->
+    {ok, "To ensure the supervisor remains active even after handling multiple updates.
+        In addition, Separating the supervisor module from the server module allows better fault tolerance and modularity in the system design."}.
--- ex2/matrix_supervisor.erl
+++ ex2/matrix_supervisor.erl
@@ -3,18 +3,22 @@
 -export([spawnServer/0]).
 
 spawnServer() ->
-    io:format("Starting supervisor~n"),
     process_flag(trap_exit, true),
-    Pid = spawn_link(matrix_server,serverLoop, [1]),
-    register(matrix_server, Pid),
-    PidString = erlang:pid_to_list(Pid),
-    io:format("Hello from supervisor! ~s~n", [PidString]),
-    supervisorLoop(Pid).
+    ServerPid = spawn_link(fun() -> matrix_server:serverLoop(1) end),
+    register(matrix_server, ServerPid),
+    io:format("Hello from supervisor!~n"),
+    supervisorLoop(ServerPid).
 
-supervisorLoop(Pid) ->
+supervisorLoop(ServerPid) ->
     receive
-        {'EXIT', Pid, normal} -> ok;
-        {'EXIT', Pid, _} ->
-           unregister(matrix_server),
-           exit(normal) 
-    end. 
+        {'EXIT', ServerPid, normal} -> ok;
+        {'EXIT', ServerPid, _} ->
+            flushInbox(),
+            supervisorLoop(ServerPid)
+    end.
+
+flushInbox() ->
+    receive
+        _ -> flushInbox()
+    after 0 -> ok
+    end.
